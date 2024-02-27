1c0d318bac675bd619d64a29347661e8dbe25908
Antoine <antoine24deboeuf@gmail.com>
2024-02-27 11:46:37 +0100

    added authentication into login page

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {
+    }
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $user = (new User)
+            ->setLastName('Deboeuf')
+            ->setFirstName('Antoine')
+            ->setEmail('antoine@test.com')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->hasher->hashPassword(new User, 'Test1234!'));
+
+        $manager->persist($user);
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = (new User)
+                ->setLastName('User')
+                ->setFirstName($i)
+                ->setEmail("user-$i@test.com")
+                ->setPassword($this->hasher->hashPassword($user, 'Test1234!'));
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
